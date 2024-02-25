54a965124924dfc3ffba4cd23853e974c9885805
y2kun <me@y2kun.dev>
2024-02-25 17:11:58 +0100

    slight changes

--- flake/nixosModules/lambda.nix
+++ flake/nixosModules/lambda.nix
@@ -24,6 +24,7 @@
       fprintd.enable = true;
       # Enable touchpad support (enabled default in most desktopManager).
       xserver.libinput.enable = true;
+      touchegg.enable = true;
     };
 
     users.users.yuma = {
