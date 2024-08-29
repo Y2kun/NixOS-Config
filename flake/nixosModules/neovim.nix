8f59e5c68b8546296e3c6ab400af321378a36d8a
Y2kun <me@y2kun.dev>
2024-08-29 18:21:22 +0200

    neovim plugins

--- flake/nixosModules/neovim.nix
+++ flake/nixosModules/neovim.nix
@@ -10,7 +10,42 @@
       # defaultEditor = true;
       plugins = with pkgs.vimPlugins; [
         nvchad
+        nvchad-ui
+        vim-nix
+        nvim-tree
+        nvim-treesitter.withPlugins
+        (lang: [
+          # lang.bash
+          lang.comment
+          lang.css
+          lang.dockerfile
+          lang.fish
+          lang.gitattributes
+          lang.gitignore
+          # lang.go
+          # lang.gomod
+          # lang.gowork
+          # lang.hcl
+          lang.javascript
+          # lang.jq
+          # lang.json5
+          lang.json
+          lang.lua
+          lang.make
+          lang.markdown
+          lang.nix
+          lang.python
+          lang.rust
+          lang.toml
+          # lang.typescript
+          # lang.vue
+          lang.yaml
+        ])
       ];
+      extraConfig = {
+      };
+      extraLuaConfig = {
+      };
     };
   };
 }
