5dc99a016d9b792ce32bd069127bdf790e3b839c
Y2kun <me@y2kun.dev>
2024-08-31 08:12:40 +0200

    changed nvim config

--- flake/nixosModules/neovim.nix
+++ flake/nixosModules/neovim.nix
@@ -12,40 +12,46 @@
         nvchad
         nvchad-ui
         vim-nix
-        nvim-tree
-        nvim-treesitter.withPlugins
-        (lang: [
-          # lang.bash
-          lang.comment
-          lang.css
-          lang.dockerfile
-          lang.fish
-          lang.gitattributes
-          lang.gitignore
-          # lang.go
-          # lang.gomod
-          # lang.gowork
-          # lang.hcl
-          lang.javascript
-          # lang.jq
-          # lang.json5
-          lang.json
-          lang.lua
-          lang.make
-          lang.markdown
-          lang.nix
-          lang.python
-          lang.rust
-          lang.toml
-          # lang.typescript
-          # lang.vue
-          lang.yaml
-        ])
+        nvim-tree-lua
+        nvim-whichkey-setup-lua
+        nvim-nonicons
+        telescope-nvim
+        gitsigns-nvim
+        gitignore-nvim
+        nvim-cmp
+        nvim-autopairs
+        # nvim-treesitter.withPlugins
+        # (lang: [
+        #   # lang.bash
+        #   lang.comment
+        #   lang.css
+        #   lang.dockerfile
+        #   lang.fish
+        #   lang.gitattributes
+        #   lang.gitignore
+        #   # lang.go
+        #   # lang.gomod
+        #   # lang.gowork
+        #   # lang.hcl
+        #   lang.javascript
+        #   # lang.jq
+        #   # lang.json5
+        #   lang.json
+        #   lang.lua
+        #   lang.make
+        #   lang.markdown
+        #   lang.nix
+        #   lang.python
+        #   lang.rust
+        #   lang.toml
+        #   # lang.typescript
+        #   # lang.vue
+        #   lang.yaml
+        # ])
       ];
-      extraConfig = {
-      };
-      extraLuaConfig = {
-      };
+      extraLuaConfig = ''
+
+      '';
     };
   };
 }
